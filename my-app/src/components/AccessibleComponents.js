5c2a5bb1921f0e59f06954b6c83e4171bd168db6
selenajwilliams <selena_williams@brown.edu>
2025-02-18 18:08:50 -0500

    hopefully finished

--- my-app/src/components/AccessibleComponents.js
+++ my-app/src/components/AccessibleComponents.js
@@ -65,17 +65,30 @@ export function AccessibilityResponse2() {
 export function AccessibilityResponse3() {
   return (
   <div>
-    <p style={{color: '#E86249', fontWeight: 'bold'}}>
+    {/* <p style={{color: '#E86249', fontWeight: 'bold'}}> */}
+    <p style={{fontWeight: 'bold'}}>
     To increase accessibility, I added a feature that allows you to filter drop-down menus by keyword via keyboard-only interaction
   </p>
   
    <p>
     This feature solves for mismatches between user needs and the real world, including: <br/>
-    
-    reducing cognitive load for people with cognitive disabilities
-    increasing learnability of product offerings for first time users
-    increasing efficiency for power-users who want to search via dropdown 
-    increasing the efficiency of keyboard and screen-reader users who want to know all menu categories that contain keyword
+    <ul>
+      <li>
+      reducing cognitive load for people with cognitive disabilities
+      </li>
+      <li>
+      increasing learnability of product offerings for first time users
+      </li>
+      <li>
+      increasing efficiency for power-users who want to search via dropdown 
+      </li>
+      <li>
+      increasing the efficiency of keyboard and screen-reader users who want to know all menu categories that contain keyword
+      </li>
+    </ul>
+
+    For a visual of what this can look like, see the Nike before and after images at the top of this page.
+
   </p>
   </div>
   )
@@ -92,7 +105,7 @@ export function ExplorationSection() {
   )
 }
 
-export function IOTableSection() {
+export function TableAndTakeaways() {
   return (
   <div>
   <div class='container'>
@@ -106,17 +119,17 @@ export function IOTableSection() {
       <p>
         <span style={{fontWeight: 'bold'}}>Takeaways:</span> <br />
         <ul style={{ listStyleType: 'disc' }}>
-                    <li>non-standardized user interaction across drop-down menus 
+                    <li>Non-standardized user interaction across drop-down menus 
                       <ul> <li>
                           e.g. some sites activate the dropdown on hover while others do only on click
                       </li> </ul>
                     </li>
-                    <li>differing levels of efficiency vs usability 
+                    <li>Differing levels of efficiency vs usability 
                       <ul><li> 
                         e.g. Google Forms allows for filtering by keyword but Nike does not
                       </li></ul>
                     </li>
-                    <li>the most accessible allowed for a diversity of ways to accomplish a task
+                    <li>The most accessible websites allowed for a diversity of ways to accomplish a task
                       <ul><li>
                         e.g. being able to activate a dropdown menu by hovering over it OR clicking on it OR  being able to navigate with keyboard via focus 
                       </li></ul>
@@ -126,27 +139,29 @@ export function IOTableSection() {
         This is <span style={{fontWeight: 'bold'}}> inaccessible </span> because: 
         <ul>
           <li>
-          site-specific differences in interaction norms creates decreased learnability, intuition, and confidence when navigating digital content 
+          Site-specific differences in interaction norms creates decreased learnability, intuition, and confidence when navigating digital content 
           </li>
           <li>
-            without a filtering option, people who use screen readers have to wait a long time to find the dropdown option they are looking for. Decreased efficiency harms access and usability.
+            Without a filtering option, people who use screen readers have to wait a long time to find the dropdown option they are looking for. Decreased efficiency harms access and usability.
           </li>
         </ul>
 
-        Examples of how this creates inaccessibility:
+        Examples of  <span style={{fontWeight: 'bold'}}>how this creates inaccessibility: </span>
         <ul>
           <li>
             Without a filtering option, people who use screen readers may have to wait a long time to get to the dropdown option they are looking for. This decrease in efficiency directly impacts usability and access.
           </li>
           <li>
-            Another example of in-accessibility are site-specific Differences around interaction norms cause people who use screen readers because without clear visual cues to communicate user-interaction modalities, users experience decreased learnability, intuition, and confidence when navigating digital content. 
+            Non-standardized interaction norms prevent people who use screenreaders from being able to develop an intuition for how to interact with dropdown menus, causing delays and frustration.
           </li>
         </ul>
       </p>
     </div>
   </div>
-  <span style={{fontWeight: 'bold'}}>This matters because...<br/></span>
-      These access disparities highlight how visually-able mouse-users are often prioritized in digital design, followed by visually-able keyboard users followed by people who use screenreaders. These differences in access go beyond good design to dictate the worlds people have access to (Holmes), necessitating investment and innovation for a world where everyone has access. 
+  <p>
+  {/* <span style={{fontWeight: 'bold', fontStyle: 'italic'}}>This matters because...<br/></span> */}
+  ☝🏽 These access disparities highlight how visually-able mouse-users are often prioritized in digital design, followed by visually-able keyboard users followed by people who use screenreaders. These differences in access go beyond good design to dictate the worlds people have access to (Holmes), necessitating investment and innovation for a world where everyone has access. 
+  </p>
   </div>
   
   )
@@ -195,7 +210,7 @@ export default function AccessibleComponents() {
 
 
 
-    <h2 className="left-aligned-text">what if you could <span style={{color: '#E86249'}}> filter </span> by key word for  <span style={{color: '#E86249'}}> faster </span> menu navigation via keyboard? </h2>
+    <h2 className="left-aligned-text" style={{fontStyle: 'italic'}}>what if you could <span style={{color: '#E86249'}}> filter </span> by key word for  <span style={{color: '#E86249'}}> faster </span> menu navigation via keyboard? </h2>
     <img className="img" src={NikeBefore} alt="dropdown menu on Nike website"/>
     <img className="img" src={NikeAfter} alt="dropdown menu on Nike website with search by keyword capability" />
 
@@ -206,12 +221,10 @@ export default function AccessibleComponents() {
     <CustomImageGallery />
 
     <h2 className="left-aligned-text"> analyzing accessibility of popular dropdown interfaces 🤔</h2>
-    <IOTableSection />
+    <TableAndTakeaways />
 
-    <h4>analyzing existing ways users can experience dropdown menus</h4>
-    <AccessibilityResponse2/>
-    <h4>designing more accessible dropdown menus</h4>
-    <h5>“Accessibility is about solving for mismatches between users and the real world” - Kat Holmes</h5>
+    <h2  className="left-aligned-text">designing more accessible dropdown menus</h2>
+    <h3>“Accessibility is about solving for mismatches between users and the real world” - Kat Holmes</h3>
     <AccessibilityResponse3/>
     </div> // main-box div
 
